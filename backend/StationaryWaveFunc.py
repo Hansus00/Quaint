87cb11f56aee13a6dbee5a6b746d70865dc6c1bb
Your Name <you@example.com>
2026-05-15 19:14:00 +0200

    gaussian wave packet implemented

--- backend/StationaryWaveFunc.py
+++ backend/StationaryWaveFunc.py
@@ -12,12 +12,17 @@ class StationaryWaveFunc:
 class GaussianPacket(StationaryWaveFunc):
     def __init__(
         self,
-        r0: tuple[float, float],
-        k0: tuple[float, float],
+        r0: tuple[int, int],
+        k0: NDArray[np.float64],
         sigma0: NDArray[np.float64],
         size_x: int,
         size_y: int,
     ):
         # TODO: fix it!
-        matrix = np.zeros((size_x, size_y), dtype=np.complex128) * (1 + 1.0j)
+        _x = np.linspace(0, size_x, size_x - 1)
+        _y = np.linspace(0, size_y, size_y - 1)
+        r = np.meshgrid(_x, _y)
+        matrix = np.exp(1j * np.einsum("iijk->jk",k0,r) - 0.5 (r-r0).T @ np.linalg.inv(sigma0) @ (r-r0))
+        matrix /= np.sum(np.abs(matrix)**2)
         super().__init__(matrix)
+        
