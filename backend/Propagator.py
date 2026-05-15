36454be4a7154963dce51cdb15ea11e30b4585c8
Jacek Winiarczyk <kancelariajkw@gmail.com>
2026-05-15 17:44:06 +0200

    More architectural changes

--- backend/Propagator.py
+++ backend/Propagator.py
@@ -3,13 +3,17 @@ from Potential import Potential
 
 
 class Propagator:
+    """
+    returns psi(t+Delta t) = U psi(t)
+    """
+
     potential: Potential
 
     def __init__(self, potential: Potential):
         self.potential = potential
 
     def __call__(
-        self, waveFunc: StationaryWaveFunc, deltaT: float = 1e-3
+        self, wave_func: StationaryWaveFunc, delta_t: float = 1e-3
     ) -> StationaryWaveFunc:
         raise NotImplementedError
 
--- backend/StationaryWaveFunc.py
+++ backend/StationaryWaveFunc.py
@@ -7,3 +7,15 @@ class StationaryWaveFunc:
 
     def __init__(self, matrix: NDArray[np.complex128]):
         raise NotImplementedError
+
+
+class GaussianPackage(StationaryWaveFunc):
+    def __init__(
+        self,
+        r0: tuple[float, float],
+        k0: tuple[float, float],
+        sigma0: tuple[float, float],
+    ):
+        # TODO: fix it!
+        matrix = np.eye(10) + np.eye(10) * 1.0j
+        super().__init__(matrix)
