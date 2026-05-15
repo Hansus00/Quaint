0fb0eaf867b8631302846a6cf4ecf557774ecafd
Jacek Winiarczyk <kancelariajkw@gmail.com>
2026-05-15 19:05:01 +0200

    Potential

--- backend/Solver.py
+++ backend/Solver.py
@@ -1,5 +1,6 @@
 from StationaryWaveFunc import StationaryWaveFunc
-from Propagator import Propagator
+from Propagator import Propagator, Cayley
+from scipy.integrate import solve_ivp
 
 
 class Solver:
@@ -17,8 +18,17 @@ class Solver:
         raise NotImplementedError
 
 
-class CrankNicolson(Solver):
-    raise NotImplementedError
+class CrankNicolson(Cayley):
+    def __init__(self):
+        pass
+
+    def __call__(
+        self,
+        wave_func: StationaryWaveFunc,
+        delta_t: float = 1e-3,
+        n_steps: int = 1,
+    ) -> StationaryWaveFunc:
+        raise NotImplementedError
 
 
 class SSFM(Solver):
