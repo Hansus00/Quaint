f9f4bc4a5dbbe543d0d1a30f480a65177ec1d124
Franciszek Hansdorfer <franciszek.hansdorfer@gmail.com>
2026-05-15 18:53:26 +0200

    Cayley propagator implementation

--- backend/Propagator.py
+++ backend/Propagator.py
@@ -1,6 +1,6 @@
 from StationaryWaveFunc import StationaryWaveFunc
 from Potential import Potential
-
+import numpy as np
 
 class Propagator:
     """
@@ -12,15 +12,39 @@ class Propagator:
     def __init__(self, potential: Potential):
         self.potential = potential
 
-    def __call__(
-        self, wave_func: StationaryWaveFunc, delta_t: float = 1e-3
-    ) -> StationaryWaveFunc:
+    def _step(self, wave_func: StationaryWaveFunc, delta_t: float = 1.e-3) -> StationaryWaveFunc:
         raise NotImplementedError
+    
+    def step_multiple(self, wave_func: StationaryWaveFunc, delta_t: float = 1.e-3, n_steps=1) -> StationaryWaveFunc:
+        raise NotImplementedError
+
+    # def __call__(
+    #     self, wave_func: StationaryWaveFunc, delta_t: float = 1e-3
+    # ) -> StationaryWaveFunc:
+    #     raise NotImplementedError
 
 
 class Cayley(Propagator):
-    raise NotImplementedError
+    
+    def __init__(self, potential):
+        super().__init__(potential)
+
+    def step(self, wave_func: StationaryWaveFunc, delta_t: float = 1.e-3) -> StationaryWaveFunc:
+        momentum_squared = wave_func.p_x **2 + wave_func.p_y ** 2
+        Hamiltonian = momentum_squared/ 2 / wave_func.mass + self.potential
+
+        complex_factor = 1j*Hamiltonian * delta_t / 2
+        factor = np.linalg.inv(1 + complex_factor)*(1 - complex_factor)
+
+        return factor * wave_func
+    
+    def step_multiple(self, wave_func: StationaryWaveFunc, delta_t: float = 1.e-3, n_steps=1) -> StationaryWaveFunc:
+        return self._step(wave_func, delta_t, n_steps)
+
+
 
 
 class SuzukiTrotter(Propagator):
-    raise NotImplementedError
+    
+    def __init__(self, potential):
+        super().__init__(potential)
