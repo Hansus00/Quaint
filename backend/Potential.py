27b241faadec36231226bcd05c193c65c591248d
Jacek Winiarczyk <kancelariajkw@gmail.com>
2026-05-15 19:05:01 +0200

    Potential

--- backend/Potential.py
+++ backend/Potential.py
@@ -10,17 +10,15 @@ class Potential:
 
 
 class InfiniteWellPotential(Potential):
-    WALL_VALUE: float = 100
-
-    def __init__(self, size_x: int, size_y: int):
+    def __init__(self, size_x: int, size_y: int, WALL_VALUE: float = 100):
         matrix = np.zeros((size_x, size_y), dtype=np.float128)
 
         for x in range(0, size_x):
-            matrix[x, 0] = self.WALL_VALUE
-            matrix[x, size_y - 1] = self.WALL_VALUE
+            matrix[x, 0] = WALL_VALUE
+            matrix[x, size_y - 1] = WALL_VALUE
 
         for y in range(0, size_y):
-            matrix[0, y] = self.WALL_VALUE
-            matrix[size_x - 1, y] = self.WALL_VALUE
+            matrix[0, y] = WALL_VALUE
+            matrix[size_x - 1, y] = WALL_VALUE
 
         super().__init__(matrix)
