b9ff5b41e4f840c1a2927684eebc5abcef10aba7
Jacek Winiarczyk <kancelariajkw@gmail.com>
2026-05-15 17:59:08 +0200

    GaussianPacket2

--- backend/StationaryWaveFunc.py
+++ backend/StationaryWaveFunc.py
@@ -6,7 +6,7 @@ class StationaryWaveFunc:
     matrix: NDArray[np.complex128]
 
     def __init__(self, matrix: NDArray[np.complex128]):
-        raise NotImplementedError
+        self.matrix = matrix
 
 
 class GaussianPacket(StationaryWaveFunc):
