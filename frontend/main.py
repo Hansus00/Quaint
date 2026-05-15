8cdd6af6b36e8b7623643de9b17628aeb6f92bb6
Adam Podkowinski <adampodkdev@gmail.com>
2026-05-15 19:40:31 +0200

    Fix potential drawing

--- frontend/main.py
+++ frontend/main.py
@@ -4,9 +4,12 @@ from PyQt6.QtWidgets import (
     QApplication,
 )
 from wave_function_simulator import WaveFunctionSimulator
+import numpy as np
+
 
 if __name__ == "__main__":
+    np.set_printoptions(threshold=np.inf)
     app = QApplication(sys.argv)
-    window = WaveFunctionSimulator()
+    window = WaveFunctionSimulator(size_x=50, size_y=60)
     window.show()
     sys.exit(app.exec())
