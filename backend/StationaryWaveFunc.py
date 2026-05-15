004fe2cccf4acc32ce7606882a519693b3bd5e7e
Franciszek Hansdorfer <franciszek.hansdorfer@gmail.com>
2026-05-15 19:55:02 +0200

    fixes in the gaussian wave packet

--- backend/StationaryWaveFunc.py
+++ backend/StationaryWaveFunc.py
@@ -18,17 +18,20 @@ class GaussianPacket(StationaryWaveFunc):
         size_x: int,
         size_y: int,
     ):
-        # TODO: fix it!
-        _x = np.linspace(0, size_x, size_x - 1)
-        _y = np.linspace(0, size_y, size_y - 1)
-        x,y = np.meshgrid(_x, _y)
+        _x = np.arange(size_x)
+        _y = np.arange(size_y)
+        
+        x, y = np.meshgrid(_x, _y, indexing="ij")
 
         dx = x - r0[0]
         dy = y - r0[1]
 
         dr = np.stack([dx, dy])
 
-        matrix = np.exp(1j * np.einsum("i,ijk->jk",k0,dr) - 0.5 * np.einsum("ikl,ij,jkl->kl",dr,np.linalg.inv(sigma0),dr))
-        matrix /= np.sum(np.abs(matrix)**2)
-        super().__init__(matrix)
+        matrix = np.exp(
+            1j * np.einsum("i,ijk->jk", k0, dr) 
+            - 0.5 * np.einsum("ikl,ij,jkl->kl", dr, np.linalg.inv(sigma0), dr)
+        )
         
+        matrix /= np.sqrt(np.sum(np.abs(matrix)**2))
+        super().__init__(matrix)        
