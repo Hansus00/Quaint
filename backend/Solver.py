3b99badad4502783646683421e33a2da7bf4b20e
Franciszek Hansdorfer <franciszek.hansdorfer@gmail.com>
2026-05-17 00:26:29 +0200

    Added the SSFM full and half step methods

--- backend/Solver.py
+++ backend/Solver.py
@@ -39,6 +39,25 @@ class Solver:
         """Returns wave function at current state at time t"""
         return self._wave_func
 
+    def _create_laplace_operator(self, Nx: int, Ny: int) -> sp.spmatrix:
+        # similiar to https://stackoverflow.com/questions/34895970/buildin-a-sparse-2d-laplacian-matrix-using-scipy-modules
+        dx, dy = 1, 1  # FIXME: which value should be a grid step?
+        D_xx = sp.diags([1, -2, 1], [-1, 0, 1], shape=(Nx, Nx)) / dx**2  # type: ignore
+        D_yy = sp.diags([1, -2, 1], [-1, 0, 1], shape=(Ny, Ny)) / dy**2  # type: ignore
+
+        I_x = sp.identity(Nx)
+        I_y = sp.identity(Ny)
+
+        # 2D Laplacian discretization consistent with C-order flattening used by numpy:
+        # index mapping (i, j) -> i * Ny + j
+        return sp.kron(D_xx, I_y) + sp.kron(I_x, D_yy)
+
+    def _create_hamilton_operator(self, L_2D: sp.spmatrix, mass: float) -> sp.spmatrix:
+        T_matrix = -(1 / (2 * mass)) * L_2D
+        V_1d = self.potential.matrix.flatten()
+        V_matrix = sp.diags(V_1d, offsets=0, format="csr")
+        return T_matrix + V_matrix
+
 
 class CrankNicolson(Solver):
     L_2D: sp.spmatrix  # TODO: to be removed
@@ -61,25 +80,6 @@ class CrankNicolson(Solver):
 
         self._wave_state_1D = self._wave_func.matrix.flatten().astype(np.complex128)
 
-    def _create_laplace_operator(self, Nx: int, Ny: int) -> sp.spmatrix:
-        # similiar to https://stackoverflow.com/questions/34895970/buildin-a-sparse-2d-laplacian-matrix-using-scipy-modules
-        dx, dy = 1, 1  # FIXME: which value should be a grid step?
-        D_xx = sp.diags([1, -2, 1], [-1, 0, 1], shape=(Nx, Nx)) / dx**2
-        D_yy = sp.diags([1, -2, 1], [-1, 0, 1], shape=(Ny, Ny)) / dy**2
-
-        I_x = sp.identity(Nx)
-        I_y = sp.identity(Ny)
-
-        # 2D Laplacian discretization consistent with C-order flattening used by numpy:
-        # index mapping (i, j) -> i * Ny + j
-        return sp.kron(D_xx, I_y) + sp.kron(I_x, D_yy)
-
-    def _create_hamilton_operator(self, L_2D: sp.spmatrix, mass: float) -> sp.spmatrix:
-        T_matrix = -(1 / (2 * mass)) * L_2D
-        V_1d = self.potential.matrix.flatten()
-        V_matrix = sp.diags(V_1d, offsets=0, format="csr")
-        return T_matrix + V_matrix
-
     def _create_cayley_matrices(
         self, N_total: int, H: sp.spmatrix
     ) -> tuple[sp.spmatrix | sp.sparray, sp.spmatrix | sp.sparray]:
@@ -92,7 +92,7 @@ class CrankNicolson(Solver):
     def step(self):
         # Crank Nicolson
         super().step()
-        self._wave_state_1D = np.asarray(spsolve(self.A, self.B @ self._wave_state_1D))
+        self._wave_state_1D = np.asarray(spsolve(self.A, self.B @ self._wave_state_1D))  # type: ignore
 
     def get_wave_function(self) -> StationaryWaveFunc:
         Nx, Ny = self.potential.matrix.shape
@@ -114,6 +114,70 @@ class Constant(Solver):
         super().step()
 
 
-class SSFM(Solver):
-    def __init__(self):
+class BaseSSFM(Solver):
+    _U_T: NDArray[np.complex128]
+    _U_V: NDArray[np.complex128]
+
+    def __init__(
+        self, potential: Potential, wave_func: StationaryWaveFunc, delta_t: float = 1e-3
+    ):
+        super().__init__(potential, wave_func, delta_t)
+
+        Nx, Ny = self.potential.matrix.shape
+        dx, dy = 1, 1  # FIXME: which value should be a grid step?
+
+        self._U_V = self._create_real_space_propagator()
+        self._U_T = self._create_momentum_propagator(Nx, Ny, dx, dy, wave_func.mass)
+
+    def _create_real_space_propagator(self) -> NDArray[np.complex128]:
         raise NotImplementedError
+
+    def _create_momentum_propagator(
+        self, Nx: int, Ny: int, dx: float, dy: float, mass: float
+    ) -> NDArray[np.complex128]:
+        """Creates the momentum space propagator."""
+        kx = np.fft.fftfreq(Nx, d=dx) * 2 * np.pi
+        ky = np.fft.fftfreq(Ny, d=dy) * 2 * np.pi
+        kx2, ky2 = np.meshgrid(kx**2, ky**2, indexing="ij")
+
+        T = (kx2 + ky2) / (2 * mass)
+        return np.exp(-1j * T * self.delta_t)
+
+
+class SSFM(BaseSSFM):
+    """Standard Split-Step Fourier Method."""
+    def _create_real_space_propagator(self) -> NDArray[np.complex128]:
+        """Creates the full-step real space propagator."""
+        return np.exp(-1j * self.potential.matrix * self.delta_t)
+    
+    def step(self):
+        super().step()
+
+        psi = self._wave_func.matrix * self._U_V
+
+        psi_k = np.fft.fft2(psi)
+        psi_k *= self._U_T
+        psi = np.fft.ifft2(psi_k)
+        
+        self._wave_func = StationaryWaveFunc(psi, self._wave_func.mass)
+
+
+class SSFMSymetric(BaseSSFM):
+    """Symmetric Split-Step Fourier Method."""
+    def _create_real_space_propagator(self) -> NDArray[np.complex128]:
+        """Creates the half-step real space propagator."""
+        return np.exp(-1j * self.potential.matrix * self.delta_t / 2)
+    
+    def step(self):
+        super().step()
+
+        psi = self._wave_func.matrix * self._U_V
+
+        psi_k = np.fft.fft2(psi)
+        psi_k *= self._U_T
+        psi = np.fft.ifft2(psi_k)
+        
+        psi *= self._U_V
+        
+        self._wave_func = StationaryWaveFunc(psi, self._wave_func.mass)
+
