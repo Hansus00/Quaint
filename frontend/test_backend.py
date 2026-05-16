346c23de61136bb6d1b8da1a66b1c836e08e2bde
Jacek Winiarczyk <kancelariajkw@gmail.com>
2026-05-16 11:44:58 +0200

    Backend as module3

--- frontend/test_backend.py
+++ frontend/test_backend.py
@@ -1,13 +1,16 @@
 import sys
 from pathlib import Path
 
+# to call backend module from frontend directory
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
+# calling backend module
 from backend.Potential import InfiniteWellPotential
 from backend.StationaryWaveFunc import GaussianPacket
 from backend.Solver import CrankNicolson
 import numpy as np
 
+# tests
 ipw = InfiniteWellPotential(10, 10, 1e5)
 cn = CrankNicolson(ipw)
 # print(cn.L_2D)
