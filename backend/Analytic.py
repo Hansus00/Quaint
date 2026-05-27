0ea3936bbeb12948e63b3da60ff17533c7f96d45
Bartosz Łakomy <b.lakomy@student.uw.edu.pl>
2026-05-27 22:42:36 +0200

    Basis expansion analytic solver for infinite well

--- backend/Analytic.py
+++ backend/Analytic.py
@@ -125,16 +125,17 @@ class GaussianInWellSolver(_AnalyticSolver):
         k0: NDArray[np.float64],
         r0: NDArray[np.float64],
         sigma0: NDArray[np.float64],
-        grid_size: float,
+        grid_size: int,
         mass: float,
         delta_t: float = 0.001,
-        grid_step=1,
+        grid_step: float = 1,
     ):
         """Warning: this is a clunker suggested solution and
         partially checked clanker code.
         sigma0 takes ndarray but only sigma0[0][0] is used as
         a standard deviation ** 2"""
         sigma0 = sigma0[0][0]
+        L = grid_size * grid_step
 
         self._jtheta_numpied = np.frompyfunc(
             lambda z, q: np.complex128(complex(jtheta(3, z, q))), 2, 1
@@ -142,21 +143,15 @@ class GaussianInWellSolver(_AnalyticSolver):
 
         gamma = lambda t: 1 / (4 * sigma0**2 * (1 + 1j * t) / (2 * mass * sigma0**2))
 
-        q = lambda t: np.exp(-4 * gamma(t) * grid_size**2)
+        q = lambda t: np.exp(-4 * gamma(t) * L**2)
 
         z_plus = (
-            lambda i, t: -2j
-            * gamma(t)
-            * grid_size
-            * (self._pos_1d - r0[i] - k0[i] * t / mass)
-            - k0[i] * grid_size
+            lambda i, t: -2j * gamma(t) * L * (self._pos_1d - r0[i] - k0[i] * t / mass)
+            - k0[i] * L
         )
         z_minus = (
-            lambda i, t: -2j
-            * gamma(t)
-            * grid_size
-            * (self._pos_1d + r0[i] + k0[i] * t / mass)
-            + k0[i] * grid_size
+            lambda i, t: -2j * gamma(t) * L * (self._pos_1d + r0[i] + k0[i] * t / mass)
+            + k0[i] * L
         )
 
         env_plus = lambda i, t: np.exp(
@@ -192,3 +187,76 @@ class GaussianInWellSolver(_AnalyticSolver):
 
     def ev_energy(self) -> float:
         return 1  # for testing purposes
+
+
+class InfiniteWellBasisSolver(_Solver):
+    def __init__(
+        self,
+        wave_func: StationaryWaveFunc,
+        Nx: int = 30,
+        Ny: int = 30,
+        delta_t: float = 0.001,
+        grid_step: float = 1,
+    ):
+        """Creates analytic solver for an arbitrary initial wavefunction
+        in an infinite potential well. Evolution is done by basis summation.
+
+        Args:
+            wave_func (StationaryWaveFunc): Initial wave function
+            Nx (int, optional): Maximal x mode considered. Defaults to 30.
+            Ny (int, optional): Maximal y mode considered. Defaults to 30.
+            delta_t (float, optional): Time step. Defaults to 0.001.
+            grid_step (float, optional): Grid spacing. Defaults to 1.
+        """
+        sizex = wave_func.matrix.shape[0]
+        sizey = wave_func.matrix.shape[1]
+
+        Lx = sizex * grid_step
+        Ly = sizey * grid_step
+
+        pos1dx = np.linspace(0, Lx, sizex)
+        pos1dy = np.linspace(0, Ly, sizey)
+
+        nxarray = np.linspace(1, Nx, Nx)
+        nyarray = np.linspace(1, Ny, Ny)
+
+        # meshing for optimization purposes
+        nxspace = np.einsum("i,j->ij", nxarray, pos1dx)
+        nyspace = np.einsum("i,j->ij", nyarray, pos1dy)
+
+        self._basis = (
+            (2 / np.sqrt(Lx))
+            * (2 / np.sqrt(Ly))
+            * np.einsum(
+                "ij,kl->ikjl",
+                np.sin(np.pi / Lx * nxspace),
+                np.sin(np.pi / Ly * nyspace),
+            )
+        )
+
+        self._coeffs = np.einsum("ijkl,kl->ij", np.conj(self._basis), wave_func.matrix)
+
+        energy1dx = np.pi**2 * nxarray**2 / (2 * wave_func.mass * Lx**2)
+        energy1dy = np.pi**2 * nyarray**2 / (2 * wave_func.mass * Ly * 2)
+
+        self._energyspace = np.add.outer(energy1dx, energy1dy)
+
+        self._wave_func = lambda t: np.einsum(
+            "ij,ijkl->kl",
+            self._coeffs * np.exp(-1j * self._energyspace * t),
+            self._basis,
+        )
+
+        self.delta_t = delta_t
+        self._mass = wave_func.mass
+
+    def update(self, n_step=1):
+        self._steps_evolved += n_step
+        return self.get_wave_function()  # time parameter is enough
+
+    def get_wave_function(self) -> StationaryWaveFunc:
+        return StationaryWaveFunc(
+            self._wave_lambda(self._steps_evolved * self.delta_t), self._mass
+        )  # simple evaluation at current time
+    def ev_energy(self):
+        return np.einsum("ij,ij", np.abs(self._coeffs)**2, self._energyspace)
