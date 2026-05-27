7e7eb8bdc25c9f68ae8690d60c384af81cf13099
Bartosz Łakomy <b.lakomy@student.uw.edu.pl>
2026-05-27 10:06:59 +0200

    added analytic gaussian in a box to analytic.py

--- backend/Analytic.py
+++ backend/Analytic.py
@@ -7,6 +7,8 @@ from .StationaryWaveFunc import StationaryWaveFunc
 from .Potential import InfiniteWellPotential
 from .Potential import Potential
 
+from mpmath import jtheta
+
 
 class _AnalyticSolver(_Solver):
     delta_t: float
@@ -33,7 +35,7 @@ class _AnalyticSolver(_Solver):
         self._grid = np.meshgrid(pos_1d, pos_1d, indexing="ij")
 
     def _stability_conditions(self):
-        pass # assumes correct solution
+        pass  # assumes correct solution
 
     def update(self, n_step=1):
         self._steps_evolved += n_step
@@ -42,7 +44,7 @@ class _AnalyticSolver(_Solver):
     def get_wave_function(self) -> StationaryWaveFunc:
         return StationaryWaveFunc(
             self._wave_lambda(self._steps_evolved * self.delta_t), self._mass
-        ) # simple evaluation at current time
+        )  # simple evaluation at current time
 
 
 class InfiniteWellSolver(_AnalyticSolver):
@@ -57,6 +59,10 @@ class InfiniteWellSolver(_AnalyticSolver):
         delta_t: float = 0.001,
         grid_step: float = 1,
     ):
+        """Infinite well stationary solution in the (0,0) corner in the [0, Lx] x [0, Ly]
+        region. The solution has the (Nx, Ny) index (it is the Nx'th mode in the x direction
+        and the Ny'th mode in the y direction). The potential field of this class
+        does not correspond to the actual potential this soulution obeys."""
         # wf is only inside the well
         well_mask = (0 <= self._grid[0] <= Lx) * (0 <= self._grid[1] <= Ly)
 
@@ -87,9 +93,9 @@ class GaussianPacketSolver(_AnalyticSolver):
         delta_t: float = 0.001,
         grid_step=1,
     ):
-        ''' Warning: Unlike GaussianPacket this class does not accept general
-            sigma. Instead, quadratic mean of the diagonal entries is taken as
-            the standard deviation'''
+        """Warning: Unlike GaussianPacket this class does not accept general
+        sigma. Instead, quadratic mean of the diagonal entries is taken as
+        the standard deviation"""
         a = np.sqrt(np.hypot(*np.diag(sigma0)) / np.sqrt(2))
         # source: https://en.wikipedia.org/wiki/Wave_packet#The_2D_case
         psi = lambda i, t: (
@@ -101,7 +107,8 @@ class GaussianPacketSolver(_AnalyticSolver):
                     - 0.5 * np.arctan(2 * t / mass / a**2)
                     - k0[i] ** 2 / 2 / mass * t
                 )
-                - (self._grid[i] - r0[i] - k0[i] * t / mass) ** 2 / (a**2 + 2j * t / mass)
+                - (self._grid[i] - r0[i] - k0[i] * t / mass) ** 2
+                / (a**2 + 2j * t / mass)
             )
         )
 
@@ -110,5 +117,64 @@ class GaussianPacketSolver(_AnalyticSolver):
         super().__init__(
             InfiniteWellPotential, wave_func, grid_size, mass, delta_t, grid_step
         )
+
     def ev_energy(self) -> float:
-        return 1 # for testing purposes
+        return 1  # for testing purposes
+
+
+class GaussianInWellSolver(_AnalyticSolver):
+    def __init__(
+        self,
+        k0: NDArray[np.float64],
+        r0: NDArray[np.float64],
+        sigma0: float,
+        grid_size: float,
+        mass: float,
+        delta_t: float = 0.001,
+        grid_step=1,
+    ):
+        '''Warning: this is a clunker suggested solution and 
+        partially checked clanker code.'''
+        self._jtheta_numpied = np.frompyfunc(
+            lambda z, q: np.complex128(complex(jtheta(3, z, q)), 2, 1)
+        )
+
+        gamma = lambda t: 1 / (4 * sigma0**2 * (1 + 1j * t) / (2 * mass * sigma0**2))
+
+        q = lambda t: np.exp(-4 * gamma(t) * grid_size**2)
+
+        z_plus = (
+            lambda i, t: -2j
+            * gamma(t)
+            * grid_size
+            * (self._grid[i] - r0[i] - k0[i] * t / mass)
+            - k0[i] * grid_size
+        )
+        z_minus = (
+            lambda i, t: -2j
+            * gamma(t)
+            * grid_size
+            * (self._grid[i] + r0[i] + k0[i] * t / mass)
+            + k0[i] * grid_size
+        )
+
+        env_plus = lambda i, t: np.exp(
+            -gamma(t) * (self._grid[i] - r0[i] - k0[i] * t / mass) ** 2
+            + 1j * k0[i] * (self._grid[i] - k0[i] * t / (2 * mass))
+        )
+
+        env_minus = lambda i, t: np.exp(
+            -gamma(t) * (self._grid[i] + r0[i] + k0[i] * t / mass) ** 2
+            + 1j * k0[i] * (self._grid[i] + k0[i] * t / (2 * mass))
+        )
+
+        theta_plus = lambda i,t: self._jtheta_numpied(z_plus(i,t),q(t))
+        theta_minus = lambda i,t: self._jtheta_numpied(z_minus(i,t),q(t))
+
+        psi_1d = lambda i,t: env_plus(i,t) * theta_plus(i,t) - env_minus(i,t) * theta_minus(i,t)
+
+        wave_func = lambda t: psi_1d(0,t) * psi_1d(1,t)
+
+        super().__init__(
+            InfiniteWellPotential, wave_func, grid_size, mass, delta_t, grid_step
+        )
