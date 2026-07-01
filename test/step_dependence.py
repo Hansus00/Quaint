699b17a1374013375260905694d425d32d641c0b
Jacek Winiarczyk <kancelariajkw@gmail.com>
2026-07-02 01:42:50 +0200

    Cleaning

--- test/step_dependence.py
+++ test/step_dependence.py
@@ -6,6 +6,10 @@ import pickle
 from typing import Any
 from copy import deepcopy
 
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
 from backend.Solver import CrankNicolson, SSFMSymmetric, SSFM
 from backend.Params import Params
 from backend.Analytic import _AnalyticSolver
